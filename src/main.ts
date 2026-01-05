a64dfa49b230d075191680f60fa140b8455ca55e
Ali Selimovic <ali.selimovic11@gmail.com>
2026-01-05 10:15:47 +0100

    swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Vacation planner')
+    .setDescription('Plan your next trip with us')
+    .setVersion('1.0')
+    .addTag('travel')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
